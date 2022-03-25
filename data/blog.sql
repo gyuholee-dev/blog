83a51a3e41596db1d4b291a822aebb4be7d2f816
LeeGyuho <andrewgyuholee@gmail.com>
2022-03-25 09:21:14 +0900

    Added testdata

---
+++ data/blog.sql
@@ -0,0 +1,66 @@
+/* 참고
+https://sql-factory.tistory.com/2063
+https://blueamor.tistory.com/618
+
+-- 최소일 경우 블로그 테이블만.
+-- 블로그, 보드, 코멘트 각각 테이블 필요
+-- 제대로 만들 경우: 관리, 유저, 카테고리, 포스트, 코멘트 등등 필요
+
+
+/* post 포스트
+idx 인덱스: 자동증가 정수
+wdate 날짜: 타임스탬프
+title 타이틀
+writer 이름
+category 분류: profile, portpolio...
+subcategory 하위분류: 일상, 게임...
+posttype: text, media, link...
+files 파일
+link 링크
+content 내용
+tags 태그(차후 추가)
+*/
+
+CREATE TABLE post (
+  idx INT AUTO_INCREMENT,
+  wdate INT,
+  title VARCHAR(80),
+  writer VARCHAR(20),
+  category VARCHAR(20),
+  subcategory VARCHAR(20),
+  posttype VARCHAR(20),
+  files VARCHAR(80),
+  link VARCHAR(80),
+  content TEXT,
+  PRIMARY KEY (idx)
+);
+
+/* board 보드
+idx 인덱스: 자동증가 정수
+wdate 날짜: 타임스탬프
+title 타이틀
+writer 이름
+email 이메일
+home 홈페이지
+pass 비밀번호
+ip 작성자 IP
+category 카테고리: profile, portpolio...
+posttype: text, media, link...
+tags 태그 
+files 파일
+content 내용
+
+*/
+
+CREATE TABLE board (
+
+);
+
+/* comment 덧글
+idx 인덱스
+wdate 날짜
+name 이름
+pwd 비밀번호
+content 내용
+비밀
+*/
---
+++ data/testdata_profile.sql
@@ -0,0 +1,95 @@
+/*
+ 프로필 데이터
+*/
+
+-- 프로필
+INSERT INTO post 
+(wdate, title, writer, category, subcategory, posttype, files, link, content) 
+VALUES (
+  UNIX_TIMESTAMP(),
+  '이규호의 프로필',
+  'LeeGyuho',
+  'profile',
+  '',
+  'text',
+  'BI-header_r.png',
+  '',
+  'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore molestiae architecto repellat eligendi, iusto magnam illum deserunt cumque unde voluptatibus tenetur nisi expedita doloremque quasi aliquid suscipit, temporibus omnis labore?'
+);
+
+---------------------------- 링크 (네개) ----------------------------
+
+INSERT INTO post 
+(wdate, title, writer, category, subcategory, posttype, files, link, content) 
+VALUES (
+  UNIX_TIMESTAMP(),
+  'PROFILE',
+  'LeeGyuho',
+  'profile',
+  'link',
+  'link',
+  'BI-header_r.png',
+  'view.php?page=profile',
+  '프로필 페이지 링크'
+);
+
+INSERT INTO post 
+(wdate, title, writer, category, subcategory, posttype, files, link, content) 
+VALUES (
+  UNIX_TIMESTAMP(),
+  'DEVELOPMENT',
+  'LeeGyuho',
+  'profile',
+  'link',
+  'link',
+  'images(1).jpg',
+  'view.php?page=portpolio',
+  '포트폴리오 페이지 링크'
+);
+
+INSERT INTO post 
+(wdate, title, writer, category, subcategory, posttype, files, link, content) 
+VALUES (
+  UNIX_TIMESTAMP(),
+  'ART & GRAPHIC',
+  'LeeGyuho',
+  'profile',
+  'link',
+  'link',
+  'images(2).jpg',
+  'https://www.artstation.com/geoflowerstudio',
+  '아트스테이션 링크'
+);
+
+INSERT INTO post 
+(wdate, title, writer, category, subcategory, posttype, files, link, content) 
+VALUES (
+  UNIX_TIMESTAMP(),
+  'GITHUB',
+  'LeeGyuho',
+  'profile',
+  'link',
+  'link',
+  'images(3).jpg',
+  'https://github.com/leegyuho-dev',
+  '깃허브 링크'
+);
+
+
+
+
+
+
+INSERT INTO post 
+(wdate, title, writer, category, subcategory, posttype, tags, files, content) 
+VALUES (
+  UNIX_TIMESTAMP(),
+  '테스트 타이틀',
+  'LeeGyuho',
+  'diary',
+  '일상',
+  'text',
+  '태그1,태그2,태그3,태그4',
+  '1648083930_testfile.jpg',
+  'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore molestiae architecto repellat eligendi, iusto magnam illum deserunt cumque unde voluptatibus tenetur nisi expedita doloremque quasi aliquid suscipit, temporibus omnis labore?'
+);
