1ebd68d63f1b5a0a96812be598da6aa9ccd266a0
LeeGyuho <andrewgyuholee@gmail.com>
2022-03-29 06:50:40 +0900

    Added user.php common case

--- pages/user.php
+++ pages/user.php
@@ -1,2 +1,17 @@
 <?php // user.php
 
+// $do 값에 따라 각각 다른 컨텐츠를 출력
+switch ($do) {
+  case 'login':
+    $content .= '로그인';
+    break;
+  case 'logout':
+    $content .= '로그아웃';
+    break;
+  case 'signup':
+    $content .= '회원가입';
+    break;
+  case 'mypage':
+    $content .= '마이페이지';
+    break;
+}
