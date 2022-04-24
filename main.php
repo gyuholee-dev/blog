987a14924659ed5f3ba41b56689e09c42de75d85
LeeGyuho <andrewgyuholee@gmail.com>
2022-04-24 11:25:47 +0900

    Updated

--- main.php
+++ main.php
@@ -1,69 +1,8 @@
 <?php // main.php
 // 초기화
 require_once 'includes/init.php';
+require_once 'includes/start.php';
 
-// 컨텐츠
-$head = '';
-$header = '';
-$nav = '';
-$content = '';
-$aside = '';
-$footer = '';
-
-// 헤드
-$head = <<<HTML
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="shortcut icon" href="favicon.ico">
-  <title>$info[siteTitle]</title>
-HTML;
-foreach ($libraries as $key => $library) {
-  foreach ($library as $lib) {
-    if ($key == 'styles') {
-      $head .= "<link rel='stylesheet' href='$lib'>";
-    } elseif ($key == 'scripts') {
-      $head .= "<script type='text/javascript' src='$lib'></script>";
-    }
-  }
-}
-
-// 헤더
-$siteUrl = ($_SERVER['HTTP_HOST']=='localhost')?'index.php':$info['siteUrl'];
-$headerLink = "<a href='$siteUrl'><img src='images/$theme[logo]'></a>";
-$loginLink = getLoginLink();
-$header_values = array(
-  '{headerLink}' => "<a href='$siteUrl'><img src='images/$theme[logo]'></a>",
-  '{loginLink}' => $loginLink
-);
-$header = file_get_contents('templates/_header.html');
-$header = strtr($header, $header_values);
-
-// 네비게이션메뉴
-foreach ($pages as $key => $conf) {
-  $active = ($page==$key)?'active':'';
-  $nav .= "<li class='$active'><a href='$MAIN?page=$key'>$conf[name]</a></li>";
-}
-$nav = '<ul class="menu main">'.$nav.'</ul>';
-
-// 사이드메뉴
-$aside = '';
-
-// 푸터
-$footer = <<<HTML
-  <p>$info[copyright]</p>
-HTML;
-
-
-// 콘텐츠
-include "pages/$action.php";
-
-
-// 콘솔로그
-$consoleLog = consoleLog();
-
-// 포스트스크립트
-$postScript = '';
 
 //------------------------ 랜더링 ------------------------
 
