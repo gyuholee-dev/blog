f0cffca1dd46ab4b31a919b45d8ad937429d7fcf
LeeGyuho <andrewgyuholee@gmail.com>
2022-05-03 20:33:18 +0900

    게시판 목록

--- includes/board.php
+++ includes/board.php
@@ -8,19 +8,49 @@ $pageCount = 0;
 $sql = "SELECT COUNT(*) FROM board";
 $res = mysqli_query($DB, $sql);
 $postCount = mysqli_fetch_row($res)[0];
+$pageCount = ceil($postCount / $items);
 
-$sql = "SELECT * FROM board";
+$sql = "SELECT * FROM board 
+        ORDER BY numb DESC 
+        LIMIT 0, $items ";
 $res = mysqli_query($DB, $sql);
 
+$list = '';
+while ($data = mysqli_fetch_assoc($res)) {
+  $numb = $data['numb'];
+  $title = $data['title'];
+  $nickname = $data['nickname'];
+  $wdate = date("Y-m-d H:i:s", $data['wdate']);
+  $secret = $data['secret'];
+  $hit = $data['hit'];
+  $list .= "
+    <tr>
+      <td>$numb</td>
+      <td>$title</td>
+      <td>$nickname</td>
+      <td>$wdate</td>
+      <td>$hit</td>
+    </tr>
+  ";
+}
 
 $board_content = <<<HTML
   <section class="board $ACT">
     <div class="header">
       <div class="title">$postCount 개의 게시물이 있습니다.</div>
     </div>
-
-
-    <div class="content"></div>
+    <div class="content">
+      <table class="table">
+        <tr>
+          <th>번호</th>
+          <th>제목</th>
+          <th>글쓴이</th>
+          <th>날짜</th>
+          <th>조회수</th>
+        </tr>
+        $list
+      </table>
+    </div>
   </section>
 
 HTML;
