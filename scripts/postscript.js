659a1320436d84578a55fe169b8916ad53b64f3c
LeeGyuho <andrewgyuholee@gmail.com>
2022-05-05 13:14:14 +0900

    Update postscript.js

--- scripts/postscript.js
+++ scripts/postscript.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", async function () {
+  console.log("DOMContentLoaded");
 
   // 로그 출력
   printLog();
+
+});
+
+window.addEventListener("load", async function() {
+  console.log("WindowLoaded");
 
 });
