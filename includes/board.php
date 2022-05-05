0e720450cfe3eadaac9871e56b4e1fb35a9a6d55
LeeGyuho <andrewgyuholee@gmail.com>
2022-05-06 00:12:58 +0900

    게시판 기능 추가

--- includes/board.php
+++ includes/board.php
@@ -3,61 +3,65 @@
 // TODO: 공지사항 pinned 구현
 // 카테고리 선택 버튼 추가
 
+function makeThread() {
+  global $DB;
 
-$items = 10;
-$page = 1;
-$postCount = 0;
-$pageCount = 0;
+  $items = 10;
+  $page = 1;
+  $postCount = 0;
+  $pageCount = 0;
 
-$sql = "SELECT COUNT(*) FROM board";
-$res = mysqli_query($DB, $sql);
-$postCount = mysqli_fetch_row($res)[0];
-$pageCount = ceil($postCount / $items);
+  $sql = "SELECT COUNT(*) FROM thread";
+  $res = mysqli_query($DB, $sql);
+  $postCount = mysqli_fetch_row($res)[0];
+  $pageCount = ceil($postCount / $items);
 
-$sql = "SELECT * FROM board 
-        ORDER BY numb DESC 
-        LIMIT 0, $items ";
-$res = mysqli_query($DB, $sql);
+  $sql = "SELECT * FROM thread 
+          ORDER BY threadid DESC 
+          LIMIT 0, $items ";
+  $res = mysqli_query($DB, $sql);
 
-$list = '';
-while ($data = mysqli_fetch_assoc($res)) {
-  $numb = $data['numb'];
-  $title = $data['title'];
-  $category = $data['category'];
-  $nickname = $data['nickname'];
-  $wdate = date("Y-m-d H:i:s", $data['wdate']);
-  $secret = $data['secret'];
-  $hit = $data['hit'];
-  $list .= "
-    <tr>
-      <td>$numb</td>
-      <td>$title</td>
-      <td>$category</td>
-      <td>$nickname</td>
-      <td>$wdate</td>
-      <td>$hit</td>
-    </tr>
-  ";
+  $list = '';
+  while ($data = mysqli_fetch_assoc($res)) {
+    $threadid = $data['threadid'];
+    $content = $data['content'];
+    $nickname = $data['nickname'];
+    $wdate = date("Y-m-d H:i:s", $data['wdate']);
+    $pinned = $data['pinned'];
+    $secret = $data['secret'];
+    $list .= "
+      <div class='thread'>
+        <div class='header'>
+          <span class='number'>#$threadid</span>
+        </div>
+        <div class='content'>
+          <div class='message'>$content</div>
+          <div class='info'>
+            <span class='username'>$nickname</span>
+            <span class='wdate'>$wdate</span>
+            <span class='buttons'>[삭제]</span>
+          </div>
+        </div>
+      </div>
+    ";
+  }
+  return $list;
 }
 
-$board_content = <<<HTML
-  <section class="board list table">
+$board_template = <<<HTML
+  <section class="list board">
     <div class="header">
       <div class="title">게시판</div>
     </div>
     <div class="content">
-      <table class="table">
-        <tr>
-          <th>번호</th>
-          <th width="100%">제목</th>
-          <th>구분</th>
-          <th>글쓴이</th>
-          <th>날짜</th>
-          <th>조회수</th>
-        </tr>
-        $list
-      </table>
+      {list}
     </div>
   </section>
 
 HTML;
+
+$board_data = array(
+  '{list}' => makeThread(),
+);
+
+$board_content = strtr($board_template, $board_data);
