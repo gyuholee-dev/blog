b214a4a70d639f99d72261de639fb06ad14db110
LeeGyuho <andrewgyuholee@gmail.com>
2022-05-08 18:42:01 +0900

    보드 테이블 변경, 쓰레드 설계 변경, 스타일 지정

--- data/blog.sql
+++ data/blog.sql
@@ -19,7 +19,8 @@ link 링크
 content 내용
 tags 태그(차후 추가)
 pinned 상단고정
-threadid 댓글 쓰레드
+secret 비밀글
+replycnt 답글수, 0이면 조회 생략
 allow 권한: ????
 */
 
@@ -36,6 +37,7 @@ CREATE TABLE post (
   tags VARCHAR(80),
   pinned BOOLEAN DEFAULT FALSE,
   secret BOOLEAN DEFAULT FALSE,
+  replycnt INT DEFAULT 0
   PRIMARY KEY(postid)
 );
 
@@ -60,74 +62,71 @@ CREATE TABLE user (
   PRIMARY KEY(userid)
 );
 
-/* board 보드
-numb 인덱스: 자동증가 정수
-wdate 날짜: 타임스탬프
-title 타이틀
-writer 이름
-category 분류: notice, general...
-content 내용
-secret 비밀글
-hits 조회수
-*/
-
-CREATE TABLE board (
-  idx INT AUTO_INCREMENT,
-  category CHAR(10),
-  wdate INT,
-  userid CHAR(20),
-  nickname VARCHAR(20),
-  content VARCHAR(140),
-  secret BOOLEAN DEFAULT FALSE,
-  PRIMARY KEY(idx)
-);
-
-INSERT INTO board 
-(category, title, wdate, userid, nickname, content)
-VALUES
-('notice', '공지사항 제목입니다', UNIX_TIMESTAMP(), 'gyuholee', 'Gyuholee', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque fuga commodi obcaecati delectus quaerat earum ad odit ducimus placeat doloremque corporis modi quia, harum cum exercitationem, veritatis velit aliquid nam.');
-INSERT INTO board 
-(category, title, wdate, userid, nickname, content)
-VALUES
-('general', '공지사항 제목입니다', UNIX_TIMESTAMP(), 'gyuholee', 'Gyuholee', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque fuga commodi obcaecati delectus quaerat earum ad odit ducimus placeat doloremque corporis modi quia, harum cum exercitationem, veritatis velit aliquid nam.');
-
-
 /*
- board
- forum
- discussion
- thread
- reply
-
 thread 쓰레드
   threadid 쓰레드 인덱스
   wdate 작성일
   userid 작성자 id
   nickname 작성자 이름
   content 본문
-  threadtype 타입: thread, comment, reply,
-  parent 부모(comment일 경우 postid, reply 일 경우 threadid) 
-  child 자식(threadid)
-  pinned 최상위 표시, type 이 thread 일 경우에만 적용
+  postid 포스트id (값이 0 이면 일반 게시물로 간주)
+  pinned 최상위 표시
   secret 비밀글, 부모가 비밀일 경우 자동으로 적용, 이외 선택.
+  replycnt 답글수, 0이면 조회 생략
 */
 
 CREATE TABLE thread (
   threadid INT AUTO_INCREMENT,
   wdate INT,
+  title VARCHAR(80),
   userid CHAR(20),
   nickname VARCHAR(20),
-  content VARCHAR(140),
-  threadtype CHAR(10) DEFAULT 'thread',
-  parent INT,
-  child INT,
+  content TEXT,
+  postid INT DEFAULT 0,
   pinned BOOLEAN DEFAULT FALSE,
   secret BOOLEAN DEFAULT FALSE,
+  replycnt INT DEFAULT 0
   PRIMARY KEY(threadid)
 );
 
 INSERT INTO thread 
-(wdate, userid, nickname, content)
+(wdate, userid, nickname, title, content)
 VALUES
-(UNIX_TIMESTAMP(), 'gyuholee', 'Gyuholee', 
-'Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque fuga commodi obcaecati delectus quaerat earum ad odit ducimus placeat dolorem');
+(UNIX_TIMESTAMP(), 'gyuholee', 'GyuhoLee', 
+'게시물 제목 게시물 제목',
+'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem sint, fugit eveniet esse doloremque enim delectus itaque debitis voluptatem asperiores voluptatum nostrum tenetur quod libero quo nesciunt dolor modi in!
+ Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem sint, fugit eveniet esse doloremque enim delectus itaque debitis voluptatem asperiores voluptatum nostrum tenetur quod libero quo nesciunt dolor modi in!
+ Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem sint, fugit eveniet esse doloremque enim delectus itaque debitis voluptatem asperiores voluptatum nostrum tenetur quod libero quo nesciunt dolor modi in!
+ Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem sint, fugit eveniet esse doloremque enim delectus itaque debitis voluptatem asperiores voluptatum nostrum tenetur quod libero quo nesciunt dolor modi in!');
+
+/*
+reply 답글
+  replyid 답글 인덱스
+  wdate 작성일
+  userid 작성자 id
+  nickname 작성자 이름
+  content 본문 (280자 제한)
+  threadid 쓰레드id
+  secret 비밀글, 부모가 비밀일 경우 자동으로 적용.
+*/
+
+CREATE TABLE reply (
+  replyid INT AUTO_INCREMENT,
+  wdate INT,
+  userid CHAR(20),
+  nickname VARCHAR(20),
+  content VARCHAR(280),
+  threadid INT,
+  secret BOOLEAN DEFAULT FALSE,
+  PRIMARY KEY(replyid)
+);
+
+INSERT INTO reply 
+(wdate, userid, nickname, threadid, content)
+VALUES
+(UNIX_TIMESTAMP(), 'gyuholee', 'GyuhoLee', 64,
+'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem sint, fugit eveniet esse doloremque enim delectus itaque debitis voluptatem asperiores voluptatum nostrum tenetur quod libero quo nesciunt dolor modi in!
+ Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem sint, fugit eveniet esse doloremque enim delectus itaque debitis voluptatem asperiores voluptatum nostrum tenetur quod libero quo nesciunt dolor modi in!
+ Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem sint, fugit eveniet esse doloremque enim delectus itaque debitis voluptatem asperiores voluptatum nostrum tenetur quod libero quo nesciunt dolor modi in!
+ Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem sint, fugit eveniet esse doloremque enim delectus itaque debitis voluptatem asperiores voluptatum nostrum tenetur quod libero quo nesciunt dolor modi in!');
+
--- includes/board.php
+++ includes/board.php
@@ -1,9 +1,69 @@
 <?php
 
-// TODO: 공지사항 pinned 구현
-// 카테고리 선택 버튼 추가
+function getButton($type, $label='', $attr=array()) : string
+{
+  $tag = 'button';
+  if ($type == 'submit' || $type == 'reset') {
+    $tag = 'input';
+  }
+  $class = 'btn';
+  if (isset($attr['class'])) {
+    $class .= ' '.$attr['class'];
+  }
 
-function makeThread() {
+  $button = "<$tag class='$class' ";
+  if ($tag != 'button' && $tag != $type) {
+    $button .= "type='$type' ";
+  }
+  foreach ($attr as $key => $value) {
+    $button .= "$key='$value' ";
+  }
+  if ($tag=='input' && !isset($attr['value'])) {
+    $button .= "value='$label' ";
+  }
+  $button .= ">";
+  if ($tag=='button') {
+    $button .= "$label</$tag>";
+  }
+  return $button;
+}
+
+// TODO: 공지사항 pinned 구현
+function getThread($data) {
+  foreach ($data as $key => $value) {
+    $$key = $value;
+  }
+  $isThread= !isset($replyid);
+  if ($isThread) {
+    $type = 'thread';
+    $postId = $threadid;
+    $postTitle = "#$postId $title";
+    $buttonReply = getButton('button', '답글', ['class'=>'min']);
+    $buttonEdit =
+      getButton('button', '수정', ['class'=>'min']).
+      getButton('button', '삭제', ['class'=>'min']);
+  } else {
+    $type = 'reply';
+    $postId = $replyid;
+    $postTitle = '';
+    $buttonReply = '';
+    $buttonEdit = getButton('button', '삭제', ['class'=>'min']);
+  }
+  $wdate = date("Y-m-d H:i:s", $data['wdate']);
+
+  $thread_data = array(
+    'type' => $type,
+    'postTitle' => $postTitle,
+    'content' => $content,
+    'buttonReply' => $buttonReply,
+    'wdate' => $wdate,
+    'nickname' => $nickname,
+    'buttonEdit' => $buttonEdit
+  );
+  return renderElement(TPL.'list_thread.html', $thread_data);
+}
+
+function makeThreadList() {
   global $DB;
 
   $items = 10;
@@ -17,51 +77,30 @@ function makeThread() {
   $pageCount = ceil($postCount / $items);
 
   $sql = "SELECT * FROM thread 
+          WHERE postid = 0
           ORDER BY threadid DESC 
           LIMIT 0, $items ";
   $res = mysqli_query($DB, $sql);
 
-  $list = '';
+  $html = "";
   while ($data = mysqli_fetch_assoc($res)) {
-    $threadid = $data['threadid'];
-    $content = $data['content'];
-    $nickname = $data['nickname'];
-    $wdate = date("Y-m-d H:i:s", $data['wdate']);
-    $pinned = $data['pinned'];
-    $secret = $data['secret'];
-    $list .= "
-      <div class='thread'>
-        <div class='header'>
-          <span class='number'>#$threadid</span>
-        </div>
-        <div class='content'>
-          <div class='message'>$content</div>
-          <div class='info'>
-            <span class='username'>$nickname</span>
-            <span class='wdate'>$wdate</span>
-            <span class='buttons'>[삭제]</span>
-          </div>
-        </div>
-      </div>
-    ";
+    $html .= '<div class="wrap chain">';
+    $html .= getThread($data);
+    // 답글
+    if ($data['replycnt'] > 0) {
+      $sql = "SELECT * FROM reply 
+              WHERE threadid = '$data[threadid]' ";
+      $reply_res = mysqli_query($DB, $sql);
+      while ($reply_data = mysqli_fetch_assoc($reply_res)) {
+        $html .= getThread($reply_data);
+      } 
+    }
+    $html .= '</div>';
   }
-  return $list;
+  return $html;
 }
 
-$board_template = <<<HTML
-  <section class="list board">
-    <div class="header">
-      <div class="title">게시판</div>
-    </div>
-    <div class="content">
-      {list}
-    </div>
-  </section>
-
-HTML;
-
 $board_data = array(
-  '{list}' => makeThread(),
+  'list' => makeThreadList(),
 );
-
-$board_content = strtr($board_template, $board_data);
+$board_content = renderElement(TPL.'board.html', $board_data);
