5186f117184c182694e37fd4010310f93d4a7af2
LeeGyuho <andrewgyuholee@gmail.com>
2022-05-11 07:36:02 +0900

    db 백업 갱신

--- backup/command.sql
+++ backup/command.sql
@@ -1,5 +1,5 @@
 # 데이터베이스 전부 백업
-$ mysqldump -u root blog > C:\Workspaces\blog\backup\blog_20220510.dump
+$ mysqldump -u root blog > C:\Workspaces\blog\backup\blog_20220511.dump
 
 # 데이터베이스 전부 리스토어
-$ mysql -u root blog < C:\Workspaces\blog\backup\blog_20220510.dump
+$ mysql -u root blog < C:\Workspaces\blog\backup\blog_20220511.dump
