2727267c2d8ddbbd0ba2d36beb3a45a308ae54ee
LeeGyuho <andrewgyuholee@gmail.com>
2022-05-16 08:47:39 +0900

    모바일 뷰포트 임시 대응, 아이콘태그 사파리 대응.

--- includes/elements.php
+++ includes/elements.php
@@ -230,7 +230,7 @@ function makeHead() : string
   $head = <<<HTML
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=0.75">
     <link rel="shortcut icon" href="$favicon">
     <title>$siteTitle</title>
     <meta name="description" content="$description">
